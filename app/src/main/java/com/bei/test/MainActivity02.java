dc203ed4873d706105e3cb4eeb1c3b105d0900f2
lzb <88601725@qq.com>
2016-10-12 15:50:22 +0800

    add a activity

---
+++ app/src/main/java/com/bei/test/MainActivity02.java
@@ -0,0 +1,13 @@
+package com.bei.test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
