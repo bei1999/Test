8bdbd182a6278f42e96fcf769ee507b8f79f544b
lzb <88601725@qq.com>
2016-10-31 18:23:35 +0800

    test permissiondispather    未正常运行（mark）

--- app/src/main/java/com/bei/test/MainActivity.java
+++ app/src/main/java/com/bei/test/MainActivity.java
@@ -1,13 +1,41 @@
 package com.bei.test;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.textView)
+    TextView textView;
+    @Bind(R.id.button)
+    Button button;
+    @Bind(R.id.go_permission_bt)
+    Button goPermissionBt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick({R.id.button, R.id.go_permission_bt})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                break;
+            case R.id.go_permission_bt:
+                startActivity(new Intent(this,PermissionTestActivity.class));
+                break;
+        }
     }
 }
