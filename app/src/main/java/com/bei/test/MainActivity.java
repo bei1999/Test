851c2b9cacf9e272ef47b03169611d31f9100bef
lzb <88601725@qq.com>
2016-11-04 17:34:24 +0800

    构建修改gradle

--- app/src/main/java/com/bei/test/MainActivity.java
+++ app/src/main/java/com/bei/test/MainActivity.java
@@ -2,6 +2,12 @@ package com.bei.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.bei.test.utils.Url;
+
+import static com.bei.test.BuildConfig.API_SERVICE_URL;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = Url.getHost();
+        Log.d("bei",url);
+
     }
 }
