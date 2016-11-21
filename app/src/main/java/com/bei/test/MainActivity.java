7a9e8cae3a77bff3f9b092868b912226959d495e
lzb <88601725@qq.com>
2016-11-21 18:34:16 +0800

    fixed build

--- app/src/main/java/com/bei/test/MainActivity.java
+++ app/src/main/java/com/bei/test/MainActivity.java
@@ -3,8 +3,12 @@ package com.bei.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.bei.test.utils.Url;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import static com.bei.test.BuildConfig.API_SERVICE_URL;
 
@@ -15,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick({R.id.button})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                break;
+        }
 
         String url = Url.getHost();
         Log.d("bei",url);
