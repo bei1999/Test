adb2a5f68294cb3274f9504ab55c94fdd3d4495b
bei1999 <bei1999@yeah.net>
2017-02-19 23:11:22 +0800

    test bubble sort

--- javalib/src/main/java/com/example/TestOne.java
+++ javalib/src/main/java/com/example/TestOne.java
@@ -11,16 +11,16 @@ import java.util.List;
  */
 
 public class TestOne {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         List<Student> studentList = new ArrayList<Student>();
-        studentList.add(new Student("lilei",21));
-        studentList.add(new Student("sada",23));
-        studentList.add(new Student("jj",13));
-        studentList.add(new Student("sada",23));
-        studentList.add(new Student("jj",13));
-        studentList.add(new Student("sada",23));
-        studentList.add(new Student("jj",16));
+        studentList.add(new Student("lilei", 21));
+        studentList.add(new Student("sada", 23));
+        studentList.add(new Student("jj", 13));
+        studentList.add(new Student("sada", 23));
+        studentList.add(new Student("jj", 13));
+        studentList.add(new Student("sada", 23));
+        studentList.add(new Student("jj", 16));
 //
 //        removeDuplicate(studentList);
 //        List<Integer> list = new ArrayList<>();
@@ -31,9 +31,6 @@ public class TestOne {
 //        list.add(9);
 //        list.add(3);
 //        list.add(7);
-//        int[] array = new int[]{1,4,2,6,9,3,7};
-//        maopao(array);
-        removeDuplicate(studentList);
 
     }
 
@@ -59,8 +56,8 @@ public class TestOne {
 
 
     public static void removeDuplicate(List<Student> list) {
-        for ( int i = 0 ; i < list.size() - 1 ; i ++ ) {
-            for ( int j = list.size() - 1 ; j > i; j -- ) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = list.size() - 1; j > i; j--) {
                 if (list.get(j).getName().equals(list.get(i).getName())) {
                     list.remove(j);
 
@@ -69,24 +66,4 @@ public class TestOne {
         }
         System.out.println(list);
     }
-
-    public static void maopao(int[] array){
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 1; j < array.length - 2; j++) {
-                System.out.println("i,j"+"==="+i+","+j);
-                if (array[i] > array[j]){
-                    int tmp = array[i];
-                    array[i] = array[j];
-                    array[j] = tmp;
-                }
-            }
-        }
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
-
-
-    }
-
-
 }
---
+++ javalib/src/main/java/com/example/sort/BubbleSortTest.java
@@ -0,0 +1,53 @@
+package com.example.sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by lzb on 17/2/19.
+ */
+
+public class BubbleSortTest {
+    public static void main(String[] args) {
+        int[] data = new int[]{1, 4, 2, 6, 9, 3, 7};
+        bubbleSort01(data);
+        bubbleSort02(data);
+
+    }
+
+    //小的元素向下移（气泡的下沉)
+    public static void bubbleSort01(int[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            System.out.println("第" + i + "次循环");
+            int temp = 0;
+            for (int j = i + 1; j < data.length; j++) {
+                System.out.println("i,j" + "===" + i + "," + j);
+                if (data[i] > data[j]) {
+                    temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(data));
+
+        }
+
+    }
+
+    //大的元素向上移（气泡的上浮)
+    public static void bubbleSort02(int[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            System.out.println("第" + i + "次循环");
+            int temp = 0;
+            for (int j = 0; j < data.length - i - 1; j++) {
+                System.out.println("j,j+1" + "===" + j + "," + (j + 1));
+                if (data[j] > data[j + 1]) {
+                    temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(data));
+
+        }
+    }
+}
